3a8658676baabd09635ee00592386f1176b48912
falstack <icesilt@outlook.com>
2017-10-15 13:07:39 +0800

    remove init validata

--- core/atom/export.py
+++ core/atom/export.py
@@ -10,13 +10,6 @@ class Atom(Nucleus, Electron):
     def index(self):
         return self.__index
 
-    @index.setter
-    def index(self, value):
-        if not isinstance(value, int):
-            raise ValueError('atom index must be an integer!')
-        if value < 1 or value > 118:
-            raise ValueError('atom index must between 1 ~ 118!')
-        self.__index = value
-
+    @property
     def name(self):
         return self.__name
